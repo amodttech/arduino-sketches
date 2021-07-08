241ace1da89358c6f19de620197bb67f22aa7d35
Aaron <aaron@amodttech.com>
2021-07-08 18:02:29 -0400

    fade and pot

--- trimPot.ino
+++ trimPot.ino
@@ -12,26 +12,34 @@
 */
 
 int potPosition;       //this variable will hold a value based on the position of the potentiometer
+int brightness = 0;
+int fadeAmount = 5;
+int led = 11;
 
 void setup()
 {
   Serial.begin(9600);       //start a serial connection with the computer
 
-  pinMode(13, OUTPUT);      //set pin 13 as an output that can be set to HIGH or LOW
+  pinMode(led, OUTPUT);      //set pin 13 as an output that can be set to HIGH or LOW
 }
 
 void loop()
 {
-  Serial.print(potPosition);
   //read the position of the pot
   potPosition = analogRead(A0);    //set potPosition to a number between 0 and 1023 based on how far the knob is turned
   Serial.println(potPosition);     //print the value of potPosition in the serial monitor on the computer
 
-  //change the LED blink speed based on the pot value
-  digitalWrite(13, HIGH);           // Turn on the LED
-  delay(potPosition+250);              // delay for as many milliseconds as potPosition (0-1023)
+    analogWrite(led, brightness);
 
-  digitalWrite(13, LOW);            // Turn off the LED
-  delay(potPosition+250);              // delay for as many milliseconds as potPosition (0-1023)
-}
+    brightness = brightness + fadeAmount;
+
+    if (brightness <= 0 || brightness >= 255) {
+        fadeAmount = -fadeAmount;
+    }
+
+    delay(potPosition);
+
+    Serial.print(brightness);
+    Serial.print(fadeAmount);
+    }
 
